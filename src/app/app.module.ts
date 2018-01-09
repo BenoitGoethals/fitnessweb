6f8038be77e02bac9039c79de42b52c68b7907a3
benoit <benoit.goethals@gmail.com>
2018-01-09 05:35:21 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NewclientComponent } from './newclient/newclient.component';
+import { PageNotFoundComponent } from './navbar/page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { FormsModule } from '@angular/forms';
+import { SheduleComponent } from './shedule/shedule.component';
 
 
+const appRoutes: Routes = [
+  { path: 'newclient', component: NewclientComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'shedule', component: SheduleComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NewclientComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+    SheduleComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    FlashMessagesModule.forRoot(),
+    BrowserModule,
+    FlashMessagesModule,
+    FormsModule,
+
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
